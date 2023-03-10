1e5b87243eaf8a7e5c04b3df3ba3ea58a342375a
Karyna <kharinakaryna.com>
2023-03-10 15:35:27 +0200

    added custom theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import { store } from "./redux/store";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6366f1",
+      dark: "#6311df",
+      light: "#bfd5ffb5",
+    },
+    secondary: {
+      main: "#11b981",
+      dark: "#056042",
+      light: "#ddf3f0d6",
+    },
+  },
+});
+
 render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
