4f23868a26a553589b9563a2a99e441dbbcf382c
Karyna <kharinakaryna.com>
2023-03-10 13:23:02 +0200

    updated useTodos hook and created useTodosCount hook

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,7 +1,42 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { TODO_TYPE } from "../enums";
+import { SelectorType } from "../types";
 import { AppDispatch, RootState } from "./store";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export const useTodos = () => useAppSelector((state) => state.todos.items);
+export const useTodos = (selector: SelectorType) => {
+  const allTodos = useAppSelector((state) => state.todos.items);
+
+  switch (selector) {
+    case TODO_TYPE.ALL: {
+      return allTodos;
+    }
+    case TODO_TYPE.ACTIVE: {
+      return allTodos.filter((item) => !item.completed);
+    }
+    case TODO_TYPE.COMPLETED: {
+      return allTodos.filter((item) => item.completed);
+    }
+    default: {
+      return allTodos;
+    }
+  }
+};
+
+export const useTodosCount = () => {
+  const todos = useTodos(TODO_TYPE.ALL);
+
+  return todos.reduce(
+    (acc, item) => {
+      item.completed ? acc.completed++ : acc.active++;
+      return acc;
+    },
+    {
+      all: todos.length,
+      active: 0,
+      completed: 0,
+    }
+  );
+};
