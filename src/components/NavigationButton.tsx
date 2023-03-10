688377ea92dc2777fce6433721dc5bbe01d7a811
Karyna <kharinakaryna.com>
2023-03-10 13:17:31 +0200

    created navigation button component

---
+++ src/components/NavigationButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+interface NavigationButtonProps {
+  label: string;
+  sx?: { [key: string]: string | number };
+  handleClickButton: () => void;
+}
+
+export const NavigationButton: React.FC<NavigationButtonProps> = ({
+  label,
+  sx = {},
+  handleClickButton,
+}) => {
+  const buttonSx = {
+    "&:hover": {
+      backgroundColor: "primary.dark",
+    },
+  };
+
+  return (
+    <Button
+      color="inherit"
+      sx={{ ...sx, ...buttonSx }}
+      onClick={handleClickButton}
+    >
+      {label}
+    </Button>
+  );
+};
+
