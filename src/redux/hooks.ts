15116cccd0823392a9b859b0b120335f408baa8c
Karyna <kharinakaryna.com>
2023-03-10 10:59:48 +0200

    refactoring

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -4,4 +4,4 @@ import { AppDispatch, RootState } from "./store";
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export const useTodos = useAppSelector((state) => state.todos.items);
+export const useTodos = () => useAppSelector((state) => state.todos.items);
